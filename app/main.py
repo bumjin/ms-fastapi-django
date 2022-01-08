f911f1052dc76f67d58fc665023604e259b2f65c
bumjin <bumjin@gmail.com>
2022-01-08 11:01:15 +0900

    Add Jinja Template

---
+++ app/main.py
@@ -0,0 +1,22 @@
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi import templating
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+print((BASE_DIR / "templates").exists())
+app = FastAPI()
+templats = Jinja2Templates(directory=BASE_DIR/'templates')
+
+# REST API
+
+@app.get("/", response_class=HTMLResponse) # http GET -> JSON
+def home_view(request: Request):
+    print(request)
+    return templats.TemplateResponse('home.html', {'request': request, "abc": "123"})
+
+
+@app.post("/") # http POST
+def home_detail_view():
+    return {"hello": "World"}    
---
+++ app/test_endpoints.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app) # r = requests.get(...)
+
+def test_get_home():
+    response = client.get("/") # r = requests.get #python request
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert "text/html" in response.headers["content-type"]
+    assert b"h1>Code On! 123</h1>" in response.content
+    assert response.text != "<h1>Hello World</h1>"
+
+def test_post_home_detail_view():
+    response = client.post("/") # r = requests.post #python request
+    assert response.status_code == 200
+    assert "application/json" in response.headers["content-type"]
+    assert response.json() == {"hello": "World"}    
