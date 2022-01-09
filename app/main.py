78dbe69ebb2c85e07c8b7daebe22f6a5e8dcddc8
bumjin <bumjin@gmail.com>
2022-01-09 23:06:25 +0900

    Image Upload Validation & Tests

--- app/main.py
+++ app/main.py
@@ -1,22 +1,65 @@
 import pathlib
-from fastapi import FastAPI, Request
-from fastapi import templating
-from fastapi.responses import HTMLResponse
+import os
+import io
+import uuid
+from functools import lru_cache
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Depends,
+    Request,
+    File,
+    UploadFile
+    )
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+from PIL import Image
 
+class Settings(BaseSettings):
+    debug: bool = False
+    echo_active: bool = False
+    class Config:
+        env_file = '.env'
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+settins=get_settings()
+DEBUG=settins.debug
 BASE_DIR = pathlib.Path(__file__).parent
-print((BASE_DIR / "templates").exists())
+UPLOAD_DIR = BASE_DIR / 'uploaded'
+
+# print((BASE_DIR / "templates").exists())
+
 app = FastAPI()
 templats = Jinja2Templates(directory=BASE_DIR/'templates')
-
+#print('DEBUG:', DEBUG)
 # REST API
 
 @app.get("/", response_class=HTMLResponse) # http GET -> JSON
-def home_view(request: Request):
-    print(request)
+def home_view(request: Request, settings: Settings = Depends(get_settings)):
+    print(settings.debug)
     return templats.TemplateResponse('home.html', {'request': request, "abc": "123"})
 
 
 @app.post("/") # http POST
 def home_detail_view():
     return {"hello": "World"}
+
+@app.post("/img-echo/", response_class=FileResponse) # http POST
+async def img_echo_view(file:UploadFile = File(...), settings: Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise HTTPException(status_code=400, detail="Invalid endpoint")
+    UPLOAD_DIR.mkdir(exist_ok=True)
+    bytes_str = io.BytesIO(await file.read())
+    try:
+        img = Image.open(bytes_str)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid image")
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix #.jpg #.png
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
+    img.save(dest)
+    return dest
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,5 +1,9 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import UPLOAD_DIR, app, BASE_DIR
+import shutil
+import time
+from PIL import Image, ImageChops
+import io
 
 client = TestClient(app) # r = requests.get(...)
 
@@ -16,3 +20,27 @@ def test_post_home_detail_view():
     assert response.status_code == 200
     assert "application/json" in response.headers["content-type"]
     assert response.json() == {"hello": "World"}
+
+valid_image_extensions = [".jpg", ".jpeg", ".png", ".gif"]
+def test_echo_upload():
+    img_saved_path = BASE_DIR / "images"
+    for path in img_saved_path.glob("*"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+
+        response = client.post("/img-echo/", files={"file": open(path, 'rb')}) # r = requests.post #python request
+        if img is None:
+            assert response.status_code == 400
+        else:
+            assert response.status_code == 200
+            r_steam = io.BytesIO(response.content)
+            echo_img = Image.open(r_steam)
+
+            #assert img.size == echo_img.size
+            difference = ImageChops.difference(img, echo_img).getbbox()
+            assert difference is None
+
+    #time.sleep(3)
+    shutil.rmtree(UPLOAD_DIR)
